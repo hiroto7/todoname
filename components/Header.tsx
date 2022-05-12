a4731c50ad087645274c300725decb4746aa25a8
hiroto7 <32275337+hiroto7@users.noreply.github.com>
2022-05-12 20:22:28 +0900

    Headerを別のコンポーネントに移動

---
+++ components/Header.tsx
@@ -0,0 +1,38 @@
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+
+const Header = () => {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand>To-Do Name</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          {session && (
+            <Button
+              variant="outline-secondary"
+              onClick={() => signOut({ callbackUrl: router.pathname })}
+            >
+              <i className="bi bi-box-arrow-left" /> ログアウト
+            </Button>
+          )}
+          <Nav>
+            <Nav.Link
+              href="https://github.com/hiroto7/todoname"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="bi bi-github" /> GitHub
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
