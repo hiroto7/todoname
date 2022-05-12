222dbd3090662e0d6885b57c9c6360f84e3c7ed3
hiroto7 <32275337+hiroto7@users.noreply.github.com>
2022-05-12 20:22:28 +0900

    Headerを別のコンポーネントに移動

---
+++ components/Layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./Header";
+
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+  <>
+    <Header />
+    <Container as="main" className="my-3">
+      {children}
+    </Container>
+  </>
+);
+
+export default Layout;
